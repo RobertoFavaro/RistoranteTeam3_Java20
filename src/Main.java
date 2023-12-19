2d4034e03d9b658a7c74829c03052d7c3a1ddc91
RobertoFavaro <robyfavaro04@gmail.com>
2023-12-19 15:56:22 +0100

    code review 19/12

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // doppiamo stampare il menù qui dentro
+        // ognugno di voi deve creare la propria classe (primiPiatti, secondi ecc ecc)
+        // che campi ci saranno? nome - prezzo
+        // che metodo deve avere ogni classe?  stampare
+        // dopo che avete finito, vi dovete chiedere ci sono campi e metodi duplicati per ogni calsse differente? classe padre portata
+        // dopo che avete stampato il tutto nel main dobbiamo fare una classe che sposta la loigica dal main all'oggetto menù
+        // dentro il main devono esserci solo le creazioni degli oggetti, no stampe ne altr, tutto deve essere inglobato in un ogetto menù e successivamente in un oggetto ristorante
+        // la classe menu deve avere un enumerato per il tipo e la possibilità di aggiuungere le portate e stamparle tramite un mewtodo stampamenu
+
+    }
+}
