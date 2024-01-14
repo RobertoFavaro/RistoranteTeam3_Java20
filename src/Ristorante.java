d6b4bf42cb9c97af87e743bde7cd94770a90ded1
RobertoFavaro <robyfavaro04@gmail.com>
2024-01-14 18:01:02 +0100

    creata classe Cliente e creati dei metodi in classe Ristorante

--- src/Ristorante.java
+++ src/Ristorante.java
@@ -1,4 +1,23 @@
 package src;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Ristorante {
+    private String tipo;
+    private List<Menu> menu;
+    private Map<String, Cliente> prenotazioni;
+
+    public Ristorante(String tipo, List<Menu> menu) {
+        this.tipo = tipo;
+        this.menu = menu;
+        this.prenotazioni = new HashMap<>();
+    }
+    public void aggiungiPrenotazione(String data, Cliente cliente){
+        prenotazioni.put(data, cliente);
+    }
+    public Cliente getPrenotazione(String data){
+        return prenotazioni.get(data);
+    }
 }
