45133ed6359be8d5d7bc5177053d4f6605724e57
RobertoFavaro <robyfavaro04@gmail.com>
2024-01-14 17:33:39 +0100

    sistemato bevande

--- src/Bevande.java
+++ src/Bevande.java
@@ -1,15 +1,7 @@
 package src;
 
 public class Bevande extends Portata{
-
-    public Bevande(String nome, String descrizione, String ingredienti, double prezzo, EnumMenu enumMenu) {
-        super(nome, descrizione, ingredienti, prezzo, enumMenu);
-    }
-
-    @Override
-    public void stampaPortate() {
-        System.out.println(getEnumMenu()+":");
-        System.out.print(getNome()+"    ");
-        System.out.println(getPrezzo());
+    public Bevande(String nome, double prezzo, EnumMenu enumMenu) {
+        super(nome, prezzo, enumMenu);
     }
 }
