41c8a72b31a4c4a54ffd10e3d779db5d23e4458b
RobertoFavaro <robyfavaro04@gmail.com>
2024-01-31 17:05:37 +0100

    creata tabella Bevande

--- src/schermadb.sql
+++ src/schermadb.sql
@@ -1,2 +1,14 @@
 //TODO per uil db dobbiamo fare un file schermadb.sql dove ognugno di voi inserirà le proprie tabelle, quelle comuni le fate insieme (menu, risto) il resto ve le dividete
 //TODO dobbiamo usare lucid app per fare il diagramma ER(entità releazione).
+CREATE TABLE Bevande (
+    ID_Bevande INT PRIMARY KEY,
+    ID_Portata INT,
+    descrizione VARCHAR(255)
+);
+ALTER TABLE Bevande ADD FOREIGN KEY (ID_Ristorante) REFERENCES Ristorante(ID_Ristorante);
+
+CREATE TABLE Antipasti (
+    ID_Antipasti INT PRIMARY KEY,
+    ID_Portata INT,
+    descrizione VARCHAR
+);
