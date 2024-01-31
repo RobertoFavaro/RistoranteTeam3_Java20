7173636223f2118dba0b15457d8f75349bab7079
DENISE.DL <denise.dl@hotmail.it>
2024-01-31 16:55:13 +0100

    CREAZIONE TABELLA SECONDI PIATTI

--- src/schermadb.sql
+++ src/schermadb.sql
@@ -1,2 +1,7 @@
 //TODO per uil db dobbiamo fare un file schermadb.sql dove ognugno di voi inserirà le proprie tabelle, quelle comuni le fate insieme (menu, risto) il resto ve le dividete
 //TODO dobbiamo usare lucid app per fare il diagramma ER(entità releazione).
+CREATE TABLE SECONDIPIATTI(
+ ID_SECONDIPIATTI INT PRIMARY KEY,
+ ID_PORTATA INT,
+ descrizione VARCHAR(255)
+);
